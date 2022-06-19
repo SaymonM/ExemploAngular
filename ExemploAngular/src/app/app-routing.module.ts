529709a752469ed58669dd68fcb7f65686254d50
SaymonM <saymonmeura@hotmail.com>
2022-06-19 18:53:24 -0400

    Atividade Online 1 e Encontro Remoto 2

--- ExemploAngular/src/app/app-routing.module.ts
+++ ExemploAngular/src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ComputadoresComponent } from './views/computadores/computadores.component';
 import { ContatosComponent } from './views/contatos/contatos.component';
+import { EletronicosComponent } from './views/eletronicos/eletronicos.component';
 import { HomeComponent } from './views/home/home.component';
+import { NovidadesComponent } from './views/novidades/novidades.component';
 
 const routes: Routes = [
   {path: "contatos", component: ContatosComponent},
-  {path: "", component: HomeComponent}
+  {path: "", component: HomeComponent},
+  {path: "novidades", component: NovidadesComponent},
+  {path: "computadores", component: ComputadoresComponent},
+  {path: "eletronicos", component: EletronicosComponent}
 ];
 
 @NgModule({
--- ExemploAngular/src/app/app.module.ts
+++ ExemploAngular/src/app/app.module.ts
@@ -1,22 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ContatosComponent } from './views/contatos/contatos.component';
 import { JogadorComponent } from './views/jogador/jogador.component';
 import { HomeComponent } from './views/home/home.component';
+import { HeaderComponent } from './views/header/header.component';
+import { FooterComponent } from './views/footer/footer.component';
+import { NovidadesComponent } from './views/novidades/novidades.component';
+import { ComputadoresComponent } from './views/computadores/computadores.component';
+import { EletronicosComponent } from './views/eletronicos/eletronicos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContatosComponent,
     JogadorComponent,
-    HomeComponent
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    NovidadesComponent,
+    ComputadoresComponent,
+    EletronicosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
